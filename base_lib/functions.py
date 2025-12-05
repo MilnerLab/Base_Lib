c2d5931fa3406125a38c72d3df01f3908311d5c0
MilnerLab <milnerlab@protonmail.com>
2025-12-05 11:22:30 -0800

    added cfCFG function

--- base_lib/functions.py
+++ base_lib/functions.py
@@ -24,3 +24,17 @@ def usCFG_projection(
     sigma = bandwidth / np.sqrt(8*np.log(2))
     # maybe not square gaussian
     return baseline + (1-baseline) * (gaussian(wavelengths, 1, carrier_wavelength, sigma, 0) * np.sin(phase + acceleration * (wavelengths_np - starting_wavelength)**2))**2
+
+def cfCFG_projection(
+#S = const +  (1-const)*(Gaussian(lambda - carrier,FWHM)*sin(phase + average*(lambda - carrier) + acceleration*(lambda - carrier)^3 )^2 )
+    wavelengths: Sequence[float],
+    carrier_wavelength: float,
+    average_frequency: float,
+    bandwidth: float,
+    baseline: float,
+    phase: float,
+    acceleration: float) -> list[float]:
+    wavelengths_np = np.array(wavelengths, dtype=float)
+    sigma = bandwidth / np.sqrt(8*np.log(2))
+    # maybe not square gaussian
+    return baseline + (1-baseline) * (gaussian(wavelengths, 1, carrier_wavelength, sigma, 0) * np.sin(phase + average_frequency*(wavelengths_np - carrier_wavelength) + acceleration * (wavelengths_np - carrier_wavelength)**3))**2
